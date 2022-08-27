36966c1cb8c6f25b084733ab83638a7b74b4c221
JHS <johs970917@gmail.com>
2022-08-27 13:15:06 +0900

    [추가] DirectX12 환경 세팅

--- Project_SDL/Project_SDL/main.cpp
+++ Project_SDL/Project_SDL/main.cpp
@@ -1,6 +1,9 @@
 //Using SDL and standard IO
 #include <SDL.h>
 #include <cstdio>
+#include <d3d12.h>
+#include <d3dx12.h>
+#include <DirectXMath.h>
 
 //Screen dimension constants
 const static int SCREEN_WIDTH = 640;
