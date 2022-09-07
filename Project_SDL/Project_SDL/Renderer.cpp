0e6e0aad647998b7b24681da889dd105313096b8
JHS <johs970917@gmail.com>
2022-09-07 21:44:45 +0900

    [수정] Inspect_4XMSAA_Quality 및 Create_Swap_Chain 함수 정의

--- Project_SDL/Project_SDL/Renderer.cpp
+++ Project_SDL/Project_SDL/Renderer.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <assert.h>
+
 #include "Renderer.h"
 
 void RendererHelper::Renderer::Create_Device()
@@ -36,4 +38,36 @@ void RendererHelper::Renderer::Get_Descriptor_Size()
 	m_rtv_descriptor_size = m_d3d12_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
 	m_dsv_descriptor_size = m_d3d12_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_DSV);
 	m_cbv_srv_descriptor_size = m_d3d12_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+}
+
+void RendererHelper::Renderer::Inspect_4XMSAA_Quality()
+{
+	D3D12_FEATURE_DATA_MULTISAMPLE_QUALITY_LEVELS ms_quality_levels;
+	ms_quality_levels.Format = m_back_buffer_format;
+	ms_quality_levels.SampleCount = 4;
+	ms_quality_levels.Flags = D3D12_MULTISAMPLE_QUALITY_LEVELS_FLAG_NONE;
+	ms_quality_levels.NumQualityLevels = 0;
+	ThrowIfFailed(m_d3d12_device->CheckFeatureSupport(D3D12_FEATURE_MULTISAMPLE_QUALITY_LEVELS, &ms_quality_levels, sizeof(ms_quality_levels)));
+
+	m_4xmsaa_quality = ms_quality_levels.NumQualityLevels;
+	assert(m_4xmsaa_quality > 0 && "Unexpected MSAA quality level");
+}
+
+void RendererHelper::Renderer::Create_Command_Objects()
+{
+	D3D12_COMMAND_QUEUE_DESC queue_desc = {};
+	queue_desc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+	queue_desc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	ThrowIfFailed(m_d3d12_device->CreateCommandQueue(&queue_desc, IID_PPV_ARGS(&m_command_queue)));
+
+	ThrowIfFailed(m_d3d12_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(m_command_allocator.GetAddressOf())));
+
+	ThrowIfFailed(m_d3d12_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_command_allocator.Get(), nullptr, IID_PPV_ARGS(m_command_list.GetAddressOf())));
+
+	m_command_list->Close();
+}
+
+void RendererHelper::Renderer::Create_Swap_Chain()
+{
+
 }
